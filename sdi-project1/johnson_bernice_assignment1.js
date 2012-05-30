d91b24aaace50983ad8b58d9df6977e875487903
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-30 14:30:44 -0400

    Set up my variables and console.log them all. Fixed errors I had using the console.log and jsFiddle webpage.

--- sdi-project1/johnson_bernice_assignment1.js
+++ sdi-project1/johnson_bernice_assignment1.js
@@ -2,9 +2,51 @@
 //May 30, 2012
 //Deliverable 1
 
-alert("Trip to Washington D.C.!");
+alert("Day one of trip to Washington D.C.");
 
-//Initial Variables
+//Required Variables
+//string
+//string with escapes
+//number
+//Boolean
 
-var
+////Initial Variables 10
+
+var timeToGetUp = "It\'s 7 am and my alarm clock is blowing up my iphone.", //string escape
+    daughterName = "Emma", //string
+    meAndEmma = "We both run downstairs and get her duffle bag packed with clothes and some toys", //string
+    getInCar = "so we get in the car and make a quick stop at the local convenience store.", //string
+    breakfast = "her favorite coffee kakes for breakfast.", //string
+    goodbye = "a big hug and kiss goodbye before my trip.", //string
+    myHouse = "I am finally back at my house", //string
+    directions ="We print out the directions from our house to the Washinton Hilton", //string
+    trip = true, //boolean
+    costOfGas = 35, //number
+    cash = 40,  //number
+    enoughGasMoney = true;  //boolean
+
+
+
+
+
+////console.log outputs
+
+console.log(timeToGetUp + " I swipe the snooze button and zzz for a minute and then think...It's vacation day YAY!");
+console.log("I get out of bed and wake up my daughter " + daughterName + "I get her dressed for school.");
+console.log(meAndEmma + " she will be taking to her dads house after school.");
+console.log(daughterName + " is all ready for school now " + getInCar);
+console.log("She picks out " + breakfast);
+console.log("I walk " + daughterName + " to the school door and give her " + goodbye);
+console.log(myHouse +  " and Esther and I finish getting all our stuff packed we need to take on our trip today.");
+console.log(directions +  " then load up the car.");
+console.log("It looks like we need gas first. Gas will cost " + "$" + costOfGas + ".");
+console.log("We have " + "$" + cash + " cash.");
+console.log ("Is it true we have enough gas money? " + enoughGasMoney);
+console.log("Is it true we are ready to go on our trip now? " + trip); 
+
+
+
+
+
+
 
