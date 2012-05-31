d20178c06883499b2d27f131b113288efc971c53
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-30 21:07:24 -0400

    fixed some errors

--- sdi-project1/johnson_bernice_assignment1.js
+++ sdi-project1/johnson_bernice_assignment1.js
@@ -18,13 +18,13 @@ var timeToGetUp = "It\'s 7 am and my alarm clock is blowing up my iphone.", //st
     getInCar = "so we get in the car and make a quick stop at the local convenience store.", //string
     breakfast = "her favorite coffee kakes for breakfast.", //string
     goodbye = "a big hug and kiss goodbye before my trip.", //string
-    myHouse = "I am finally back at my house", //string
-    directions ="We print out the directions from our house to the Washinton Hilton", //string
+    myHouse = "I am finally back at my house.", //string
+    directions = "We print out the directions from our house to the Washington Hilton", //string
     trip = true, //boolean
     costOfGas = 35, //number
     cash = 40,  //number
     enoughGasMoney = true,  //boolean
-    gasMoneyLeft = 0  //number
+    gasMoneyLeft = 0, //number
     placeGoingTo = "Washington";
 
 
@@ -32,33 +32,33 @@ var timeToGetUp = "It\'s 7 am and my alarm clock is blowing up my iphone.", //st
 ////console.log outputs
 
 console.log(timeToGetUp + " I swipe the snooze button and zzz for a minute and then think...It's vacation day YAY!");
-console.log("We are going to" + placeGoingTo+ ".");
-console.log("I get out of bed and wake up my daughter " + daughterName + "I get her dressed for school.");
+console.log("We are going to " + placeGoingTo + ".");
+console.log("I get out of bed and wake up my daughter " + daughterName + " and I get her dressed for school.");
 console.log(meAndEmma + " she will be taking to her dads house after school.");
 console.log(daughterName + " is all ready for school now " + getInCar);
 console.log("She picks out " + breakfast);
 console.log("I walk " + daughterName + " to the school door and give her " + goodbye);
-console.log(myHouse +  " and Esther and I finish getting all our stuff packed we need to take on our trip today.");
+console.log(myHouse +  " Esther and I finished getting all our things packed for our trip today.");
 console.log(directions +  " then load up the car.");
 console.log("It looks like we need gas first. Gas will cost " + "$" + costOfGas + ".");
 console.log("We have " + "$" + cash + " cash.");
-console.log ("Is it true we have enough gas money? " + enoughGasMoney);
+console.log("Is it true we have enough gas money? " + enoughGasMoney);
 console.log("Is it true we are ready to go on our trip now? " + trip); 
 
-if (enoughGasMoney === true){
+if (enoughGasMoney === true) {
 	console.log("We have enough money for gas, now we can go get it.");
-	if(cash > costOfGas){
+	if (cash > costOfGas){
 		gasMoneyLeft = cash - costOfGas;
 		console.log("We will have $ " + gasMoneyLeft + " left after we buy gas, cool we can both get munster drinks now.");
-	}else{
+	} else {
         console.log("No munster drinks for us. darn.");
 	};
 
-}else{
-	if (trip === true){
+} else {
+	if (trip === true) {
 		console.log("We are finally ready to drive to Washington D.C. now. YAY!");
 
-	}else{
+	} else {
 		console.log("Looks like we still have to wait longer until we can go to" + placeGoingTo + "now.");
 	};
 console.log("We are on our way to the freeway!");	
